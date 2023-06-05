830e0bfd5298b3f90333ccb4db40340848b29c08
stringertheory <mike.stringer.internet@gmail.com>
2023-06-05 15:21:02 -0500

    yay

--- bunga/yomama.py
+++ bunga/yomama.py
@@ -12,4 +12,6 @@ def shee(a: int, b: int) -> int:
     counter["alice"] += 1
     counter["alice"] += 1
     counter["bob"] += 1
-    return counter.total()
+    # not using counter.total() to be compatible with python < 3.10
+    result = sum(counter.values())
+    return result
